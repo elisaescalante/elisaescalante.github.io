08b454e3c813e8411af854d709ce3093414e5240
elisaescalante <76867622+elisaescalante@users.noreply.github.com>
2021-01-03 22:36:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-$(document).ready(function(){
-  newQuote();
-});
+function randomRange(myMin, myMax) {
+  return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
+}
 
-var newQuote = function(){
-  var index = Math.floor(Math.random() * quotes.length);
-  
-  $('span.quote').html('"' + quotes[index] + '"');
-  
-  $('a.twitter-share-button').attr('href', twitURI(quotes[index]));
-};
 
-                                  var quotes = [
+var quotes = [
 "At the end of the day, I am left wanting to unlearn what I have learned. I want to push out the new, that is now permanently tangled within my mind. I am left with new scars every day.",
 "When you are forced to find peace in chaos and suffering, you will be chaotic even in peace. You can take the human out of the nightmare, but the nightmare is still in them.",
 "No one is entitled to hear another person's truth, a person telling you their truth is always an unearned privilige.",
@@ -19,3 +12,64 @@ var newQuote = function(){
 "I am fighting, every day, just to get myself back. Not even to succeed or thrive, just to live. We are strongest when we seem weak. People will see &ldquo;helpless.&ldquo; What they won't see is the millions of battles you are fighting within yourself, just to be okay.
 "Being forced, controlled, and coerced is a lifestyle that humans are very much capable of adapting to. They can sustain life in chaos and, upon absence of it, find themselves lost."                               
 ]
+
+
+];
+
+var randomNum = randomRange(0, quotes.length-1);
+
+var randomNum2 = randomRange(0, quotes.length-1);
+
+$(document).ready(function() {
+      $("button").css("color", "white");
+      $("#text").html('<strong>"' + quotes[randomNum] + '"</strong>');
+  document.getElementById("tweet-quote").href = 'https://twitter.com/intent/tweet?text=' + quotes[randomNum];
+  
+});
+
+
+document.getElementById("text").addEventListener("click", click2);
+
+
+function click2() {
+  var randomNum = randomRange(0, quotes.length-1);;
+  
+  if(randomNum==0){
+  document.getElementById("text").innerHTML = '<strong>"' + quotes[1] + '"</strong>';
+  document.getElementById("tweet-quote").href = 'https://twitter.com/intent/tweet?text=' + quotes[1];
+  }
+ if(randomNum==1){
+  document.getElementById("text").innerHTML = '<strong>"' + quotes[2] + '"</strong>';
+    document.getElementById("tweet-quote").href = 'https://twitter.com/intent/tweet?text=' + quotes[2];
+}
+   if(randomNum==2){
+  document.getElementById("text").innerHTML = '<strong>"' + quotes[3] + '"</strong>';
+  document.getElementById("tweet-quote").href = 'https://twitter.com/intent/tweet?text=' + quotes[4];
+}
+ if(randomNum==3){
+  document.getElementById("text").innerHTML = '<strong>"' + quotes[4] + '"</strong>';
+  document.getElementById("tweet-quote").href = 'https://twitter.com/intent/tweet?text=' + quotes[4];
+}
+   if(randomNum==4){
+  document.getElementById("text").innerHTML = '<strong>"' + quotes[5] + '"</strong>';
+  document.getElementById("tweet-quote").href = 'https://twitter.com/intent/tweet?text=' + quotes[4];
+}
+    if(randomNum==5){
+  document.getElementById("text").innerHTML = '<strong>"' + quotes[6] + '"</strong>';
+  document.getElementById("tweet-quote").href = 'https://twitter.com/intent/tweet?text=' + quotes[4];
+}
+   if(randomNum==6){
+  document.getElementById("text").innerHTML = '<strong>"' + quotes[0] + '"</strong>';
+  document.getElementById("tweet-quote").href = 'https://twitter.com/intent/tweet?text=' + quotes[0];
+}
+    if(randomNum>6){
+  document.getElementById("text").innerHTML = '<strong>"' + quotes[randomNum] + '"</strong>';
+  document.getElementById("tweet-quote").href = 'https://twitter.com/intent/tweet?text=' + quotes[randomNum];
+}
+}
+
+ 
+
+
+
+
