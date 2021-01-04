6a10a50d18971f865686b79e7d4ec2407756a190
elisaescalante <76867622+elisaescalante@users.noreply.github.com>
2021-01-03 23:05:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ var quotes = [
 "No one is entitled to hear another person's truth, a person telling you their truth is always an unearned privilige.",
 "Silence was crucial to save my fragile mind. And now, my words are meaningless and empty. They aren't really me, just rehearsed. my thoughts were cut off from the world. I didn't know how to speak them. I did what I must to survive. I adapted. But now, I can't go back.",
 "And what is going to happen when it is all said and done, everything is gone, and you forgot yourself?",
-"I am fighting, every day, just to get myself back. Not even to succeed or thrive, just to live. We are strongest when we seem weak. People will see &ldquo;helpless.&ldquo; What they won't see is the millions of battles you are fighting within yourself, just to be okay.
+"I am fighting, every day, just to get myself back. Not even to succeed or thrive, just to live. We are strongest when we seem weak. People will see &ldquo;helpless.&ldquo; What they won't see is the millions of battles you are fighting within yourself, just to be okay.",
 "Being forced, controlled, and coerced is a lifestyle that humans are very much capable of adapting to. They can sustain life in chaos and, upon absence of it, find themselves lost."                               
 ]
 
