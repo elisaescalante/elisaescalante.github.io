588423d589f40913f1fa90e1c50864b68c7b0ee1
elisaescalante <76867622+elisaescalante@users.noreply.github.com>
2021-03-26 15:43:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ var newQuote = function(){
 var quotes = [
 "At the end of the day, I am left wanting to unlearn what I have learned. I want to push out the new, that is now permanently tangled within my mind. I am left with new scars every day.",
 "When you are forced to find peace in chaos and suffering, you will be chaotic even in peace. You can take the human out of the nightmare, but the nightmare is still in them.",
-"No one is entitled to hear another person's truth, a person telling you their truth is always an unearned privilige.",
+"No one is entitled to hear another person's truth, a person telling you their truth is always an unearned privilege.",
 "Silence was crucial to save my fragile mind. And now, my words are meaningless and empty. They aren't really me, just rehearsed. My thoughts were cut off from the world. I didn't know how to speak them. I did what I must to survive. I adapted. But now, I can't go back.",
 "And what is going to happen when it is all said and done, everything is gone, and you forgot yourself?",
 "I am fighting, every day, just to get myself back. Not even to succeed or thrive, just to live. We are strongest when we seem weak. People will see &ldquo;helpless.&ldquo; What they won't see is the millions of battles you are fighting within yourself, just to be okay.",
